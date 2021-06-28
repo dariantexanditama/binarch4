06c9d8bc0a92f9bfbb8baeebed1578258672f17f
dariantexanditama <thewwer@gmail.com>
2021-06-28 16:43:09 +0700

    fix js

--- binar/router.js
+++ binar/router.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const port = 3000;
 
-app.use(express.static('assets'))
+app.use(express.static('public'))
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'))
